b2341dc83ff6d0bcf651587e0cfd94873319758b
Tobias Rentschler <tobias-rentschler@gmx.de>
2026-09-03 08:48:38 +0000

    refactor: remove domain_partition_3D plain copy ahead of submodule

--- domain_partition_3D/dp3d/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Block-structured quad domain partition of cylindrical turbomachinery
-surfaces (hub/shroud): extraction from MSH/STL, cylinder unwrap to 2D,
-cross-field guided partition (ta/tb/xiao) with TFI fill."""
